ed370c8444685f18676e1c15093976f410b07e28
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-12 17:49:46 +0500

    Using sockets with express

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const server = require('http').createServer();
+const api = require('./api');
+
+const server = require('http').createServer(api);
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
@@ -6,31 +8,11 @@ const io = require('socket.io')(server, {
     }
 });
 
+const { listenSocket } = require('./sockets');
+
 const PORT = 3000;
 
 server.listen(PORT);
+listenSocket(io);
 console.log(`Listening on port ${PORT}...`);
 
-let readyPlayerCount = 0;
-
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-
-    socket.on('ready', () => {
-        console.log('Player ready', socket.id);
-
-        readyPlayerCount++;
-
-        if (readyPlayerCount === 2) {
-            io.emit('startGame', socket.id);
-        }
-    });
-
-    socket.on('paddleMove', (paddleData) => {
-        socket.broadcast.emit('paddleMove', paddleData);
-    });
-
-    socket.on('ballMove', (ballData) => {
-        socket.broadcast.emit('ballMove', ballData);
-    })
-});
