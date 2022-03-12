7c1a26f0bc37e6af9436aa1e1b15f0feacf3854c
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-12 17:16:49 +0500

    paddle synced in multiplayer mode

--- javascripts/script.js
+++ javascripts/script.js
@@ -180,6 +180,9 @@ function startGame() {
     if (paddleX[paddleIndex] > (width - paddleWidth)) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
+    socket.emit('paddleMove', {
+      xPosition: paddleX[paddleIndex]
+    })
     // Hide Cursor
     canvas.style.cursor = 'none';
   });
@@ -197,4 +200,9 @@ socket.on('startGame', (refereeId) => {
 
   isReferee = socket.id === refereeId;
   startGame();
+})
+
+socket.on('paddleMove', (paddleData) => {
+  const opponentPaddleIndex = 1 - paddleIndex
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
 })
